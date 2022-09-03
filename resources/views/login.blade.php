5ced977b0f6816513ae7c2e5121cfcfbcab11798
Aprea <apreak@gmail.com>
2022-09-03 12:35:38 +0700

    fix card tabrakan

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,7 +16,7 @@
       <div class="container">
         <div class="row">
           <div class="col-12 col-md-8">
-            <div class="card card-round my-5 mx-2">
+            <div class="card card-tracking my-5 mx-2">
               <div class="card-body my-4">
                 <div class="mx-4">
                   <h3>Lihat progres tiket</h3>
